06dcc1a8e17b906d1b280fcc91859d461f4f0d82
Sam Wong <sam@hellosam.net>
2026-09-24 06:29:22 +0800

    build: add TypeScript project tooling

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+  { ignores: ["dist/**", "node_modules/**"] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: { globals: globals.node },
+  },
+);
